66b3fba9bafb802f62122130f09b0f71b76bebf4
Nathan Guimarães <33322455+nathanguimaraes@users.noreply.github.com>
2024-05-17 13:37:09 -0300

    tratamento de erros e validacao

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
+    provideHttpClient(withFetch())
+  ]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { DefaultLoginLayoutComponent } from '../../components/default-login-layo
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +14,9 @@ import { Router } from '@angular/router';
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -20,7 +25,9 @@ export class LoginComponent {
     loginForm!: FormGroup;
 
     constructor(
-      private router: Router
+      private router: Router,
+      private loginService: LoginService,
+      private toastService: ToastrService
       ){
       this.loginForm = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
@@ -30,8 +37,12 @@ export class LoginComponent {
     }
 
     submit(){
-      console.log(this.loginForm.value)
+      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+        next: () => this.toastService.success("Login feito com sucesso!"),
+        error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
+      })
     }
+  
 
     navigate(){
       this.router.navigate(["/signup"]) //ao clicar no botao para criar conta retornara um erro de rota inexistente
